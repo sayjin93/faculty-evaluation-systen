7c1e54e8e67bef7800cdd202f0fd584ab869a1f5
Jurgen Kruja <jurgen-kruja@live.com>
2023-02-06 03:18:24 +0100

    Minor changes on footer

--- src/components/layout/AppFooter.js
+++ src/components/layout/AppFooter.js
@@ -1,23 +1,23 @@
-import React from 'react'
-import { CFooter } from '@coreui/react'
+import React from "react";
+import { CFooter } from "@coreui/react";
 
 const AppFooter = () => {
   return (
     <CFooter>
       <div>
-        <a href="https://coreui.io" target="_blank" rel="noopener noreferrer">
-          CoreUI
+        <a href="https://uet.edu.al/" target="_blank" rel="noopener noreferrer">
+          UET
         </a>
-        <span className="ms-1">&copy; 2022 creativeLabs.</span>
+        <span className="ms-1">&copy; 2023.</span>
       </div>
       <div className="ms-auto">
-        <span className="me-1">Powered by</span>
-        <a href="https://coreui.io/react" target="_blank" rel="noopener noreferrer">
-          CoreUI React Admin &amp; Dashboard Template
+        <span className="me-1">Developed by</span>
+        <a href="https://jkruja.com" target="_blank" rel="noopener noreferrer">
+          Jurgen Kruja
         </a>
       </div>
     </CFooter>
-  )
-}
+  );
+};
 
-export default React.memo(AppFooter)
+export default React.memo(AppFooter);
