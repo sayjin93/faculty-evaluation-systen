a930ca523fe92ff38f7c23536930a5b7cdc36a47
Jurgen Kruja <jurgen-kruja@live.com>
2023-02-15 12:51:23 +0100

    Translations, Change Header, .env port

--- src/layout/components/AppHeader.js
+++ src/layout/components/AppHeader.js
@@ -1,7 +1,7 @@
 import React from "react";
-import { NavLink } from "react-router-dom";
-import { useSelector, useDispatch } from "react-redux";
 
+//redux
+import { useSelector, useDispatch } from "react-redux";
 import { changeState } from "../../store/reducers/sidebarSlice";
 
 import {
@@ -10,53 +10,36 @@ import {
   CHeaderBrand,
   CHeaderNav,
   CHeaderToggler,
-  CNavLink,
-  CNavItem,
   CImage,
 } from "@coreui/react";
 import CIcon from "@coreui/icons-react";
-import { cilList, cilMenu } from "@coreui/icons";
+import { cilMenu } from "@coreui/icons";
 
 import AppHeaderDropdown from "./AppHeaderDropdown";
 import logoBlue from "../../assets/images/logo_blue.svg";
 
 const AppHeader = () => {
+  //#region redux
   const dispatch = useDispatch();
-
+  // @ts-ignore
   const show = useSelector((state) => state.sidebar.show);
+  //#endregion
 
   return (
     <CHeader position="sticky" className="mb-4">
       <CContainer fluid>
+        {/* Sidebar Toggle button */}
         <CHeaderToggler
           className="ps-1"
           onClick={() => dispatch(changeState(!show))}
         >
           <CIcon icon={cilMenu} size="lg" />
         </CHeaderToggler>
-        <CHeaderBrand className="mx-auto d-md-none" to="/">
+        {/* Brand logo in center for mobile */}
+        <CHeaderBrand className="mx-auto d-md-none" href="/">
           <CImage src={logoBlue} height={36} />
         </CHeaderBrand>
-        <CHeaderNav className="d-none d-md-flex me-auto">
-          <CNavItem>
-            <CNavLink to="/" component={NavLink}>
-              Dashboard
-            </CNavLink>
-          </CNavItem>
-          <CNavItem>
-            <CNavLink href="#">Users</CNavLink>
-          </CNavItem>
-          <CNavItem>
-            <CNavLink href="#">Settings</CNavLink>
-          </CNavItem>
-        </CHeaderNav>
-        <CHeaderNav>
-          <CNavItem>
-            <CNavLink href="#">
-              <CIcon icon={cilList} size="lg" />
-            </CNavLink>
-          </CNavItem>
-        </CHeaderNav>
+        {/* User menu */}
         <CHeaderNav className="ms-3">
           <AppHeaderDropdown />
         </CHeaderNav>
