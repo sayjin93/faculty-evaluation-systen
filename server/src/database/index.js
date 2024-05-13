d6eb5298ddd011e036627a12c53142bb0990eb1a
Jurgen Kruja <jurgen-kruja@live.com>
2024-05-13 03:14:50 +0200

    Completed #20

--- server/src/database/index.js
+++ server/src/database/index.js
@@ -1,5 +1,17 @@
 const { Sequelize } = require('sequelize');
 
+// Function declaration moved to root
+async function runSeeders(seeds) {
+  // Using Promise.all with Array#map for parallel execution (if order doesn't matter)
+  await Promise.all(seeds.map((seed) => seed()));
+
+  // If order does matter, use Array#reduce to chain promises
+  await seeds.reduce(
+    (promise, seed) => promise.then(() => seed()),
+    Promise.resolve(),
+  );
+}
+
 // Create a new Sequelize instance with database configuration
 const sequelize = new Sequelize(
   process.env.DB_NAME,
@@ -13,20 +25,18 @@ const sequelize = new Sequelize(
 
 (async () => {
   try {
-    // Authenticate the connection to the database
     await sequelize.authenticate();
     console.log('Connection has been established successfully.'.bgBlue);
 
-    // Synchronize the models with the database (alter: true means it will alter tables if they already exist)
     await sequelize.sync({
       alter: true,
       // force: process.env.NODE_ENV === 'development',
       logging: false,
     });
+
     console.log('Models synchronized'.bgGreen);
 
     if (process.env.DEFAULT_DATA === 'true') {
-      // Define an array of seed functions (ordered list)
       const seeds = [
         require('./seeders/admin'),
         require('./seeders/faculty'),
@@ -41,20 +51,16 @@ const sequelize = new Sequelize(
         require('./seeders/settings'),
       ];
 
-      // Execute all seed functions in parallel using Promise.all
-      await Promise.all(seeds.map((seed) => seed()));
+      await runSeeders(seeds);
       console.log('Seeding completed successfully!'.bgGreen);
     } else {
-      // Seed admin data
       const adminSeeder = require('./seeders/admin');
       await adminSeeder();
-      console.log('Admin seeding completed successfully!'.bgGreen);
+      console.log('Admin seeding completed successfully!');
     }
   } catch (error) {
-    // Handle any errors that occur during the process
     console.log('Unable to connect to the database:', error);
   }
 })();
 
-// Export the sequelize instance for use in other parts of the application
 module.exports = sequelize;
