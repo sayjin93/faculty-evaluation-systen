70916db99200940d62ef8b369796412490a558d4
Jurgen Kruja <jurgen-kruja@live.com>
2024-09-19 00:07:37 +0200

    Cluster Mode in pm2

--- server/ecosystem.config.js
+++ server/ecosystem.config.js
@@ -1,9 +1,13 @@
 module.exports = {
-  apps: [{
-    name: 'uet-api',
-    script: './src/index.js',
-    watch: ['src'],
-    watch_delay: 1000,
-    ignore_watch: ['node_modules'],
-  }],
+  apps: [
+    {
+      name: "uet-api",
+      script: "./src/index.js",
+      instances: "max",
+      exec_mode: "cluster",
+      watch: ["src"],
+      watch_delay: 1000,
+      ignore_watch: ["node_modules"],
+    },
+  ],
 };
